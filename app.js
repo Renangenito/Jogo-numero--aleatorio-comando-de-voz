a0bf38daecc7029118f36e3f549fd2006c59c114
renan lima <renangenito@hotmail.com>
2024-07-04 09:24:40 -0300

    Teste para funcionar no mobile

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.0});
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secreto - Web e Mobile');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 }
 
 exibirMensagemInicial();
